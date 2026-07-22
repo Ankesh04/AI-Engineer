a97ae6206ced6ea41ec998866cc06fe310194ad2
Ankesh04 <osankeshagarwal4@gmail.com>
2026-07-23 00:32:54 +0530

    Completed learning ReAct Loop

--- Prompt Engineering/ReAct_chain.py
+++ Prompt Engineering/ReAct_chain.py
@@ -1,38 +1,37 @@
 import os
 from pathlib import Path
+from time import sleep
 from dotenv import load_dotenv
 from groq import Groq
-from pydantic import BaseModel
+import re
 
 load_dotenv()
-my_api_key = os.getenv("GROQ_API_KEY")
+my_api_key=os.getenv("GROQ_API_KEY")
+
 if not my_api_key:
-    raise ValueError("API Not Found!")
+    raise ValueError("API key kaha hai bhai")
 
-client = Groq(api_key = my_api_key)
-model = "llama-3.3-70b-versatile"
+client=Groq(api_key=my_api_key)
+model="llama-3.3-70b-versatile"
 
-# Tools
-# General tool without api just functions
 def get_product_price(product):
     if product == 'iPhone 17':
         return 1000
-    elif product == 'iPhone 15':
+    elif product == "iPhone 15":
         return 500
     else:
         return 0
-
+    
 def calculator(expression):
     try:
         return eval(expression)
     except:
-        return "calculation error!"
+        return "calc error!"
 
 tools = {
     "get_product_price": get_product_price,
     "calculator": calculator
 }
-
 system_prompt = """
 You are a shopping assistant.
 
@@ -44,21 +43,20 @@ IMPORTANT:
 Call tools exactly like these examples:
 
 Action: get_product_price("iPhone 17")
-Action: calculator("5000-1000")
+Action: calculator("5000 - 1000")
 
-Never Write:
+Never write:
 get_product_price(product="iPhone 17")
 
-Never Write:
-calculator(expression="5000-1000")
-
+Never write:
+calculator(expression="5000 - 1000")
 Follow these rules:
 
 1. Decide what you need to do next.
-2. Call only one tool at a time.
+2. Call ONLY ONE tool at a time.
 3. After writing an Action, STOP immediately.
 4. Never guess or invent a tool result.
-5. Wait untill you recieve an Observation.
+5. Wait until you receive an Observation.
 6. Then decide your next action.
 7. When the task is complete, give the Final Answer.
 
@@ -77,14 +75,114 @@ def run_agent(question):
     messages = [
         {
             "role": "system",
-            "content": system_prompt#the prompt written above
+            "content": system_prompt
         },
         {
             "role": "user",
             "content": question
         }
     ]
-
+    # Here only 2 tools are used so we assume that
+    # this process can be used 5 times generate the final answer
+    # if there was 20 tools we may go for 20 loops(based on complexity)
     for step in range(5):
 
-        print("")
+        print("\n------------------")
+        print("STEP", step + 1)
+        print("------------------")
+
+        response = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=messages,
+            temperature=0
+        )
+
+        answer = response.choices[0].message.content
+
+        print(answer)
+
+        # Agent has finished
+        if "Final Answer:" in answer:
+            break
+
+
+        # Find the Action
+        match = re.search(
+            r"Action:\s*(\w+)\((.*?)\)",
+            answer
+        )
+
+
+        if match:
+
+            tool_name = match.group(1)
+
+            tool_input = match.group(2)
+
+            tool_input = tool_input.strip()
+
+            tool_input = tool_input.strip('"')
+
+
+            # Run the tool
+            if tool_name in tools:
+
+                tool = tools[tool_name]
+
+                observation = tool(tool_input)
+
+            else:
+
+                observation = "Tool not found"
+
+
+            print(
+                "Observation:",
+                observation
+            )
+
+
+            # Add LLM response to memory
+            messages.append({
+                "role": "assistant",
+                "content": answer
+            })
+
+
+            # Give tool result back to LLM
+            messages.append({
+                "role": "user",
+                "content":
+                    "Observation: "
+                    + str(observation)
+            })
+            sleep(5)
+
+
+
+prompt="""
+I have 5000 rupees. What is the price of an iphone 17?
+and how much money will I have left?
+"""
+run_agent(prompt)
+
+#output 
+# # ------------------
+# STEP 1
+# ------------------
+# Thought: First, I need to find the price of an iPhone 17 to determine how much it will cost.
+# Action: get_product_price("iPhone 17")
+# Observation: 1000
+
+# ------------------
+# STEP 2
+# ------------------
+# Thought: Now that I know the price of an iPhone 17, I need to calculate how much money will be left after buying it. To do this, I'll subtract the price of the iPhone from the initial amount of money.
+# Action: calculator("5000 - 1000")
+# Observation: 4000
+
+# ------------------
+# STEP 3
+# ------------------
+# Thought: I now have all the necessary information. The price of an iPhone 17 is 1000 rupees, and after buying it, I will have 4000 rupees left.
+# Final Answer: The price of an iPhone 17 is 1000 rupees, and you will have 4000 rupees left.
