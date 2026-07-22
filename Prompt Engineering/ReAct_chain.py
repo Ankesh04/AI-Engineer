472824a9a8e3a03e1a73a7bb4daa5138fa21a966
Ankesh04 <osankeshagarwal4@gmail.com>
2026-07-22 23:24:21 +0530

    Learned ReAct Loop for LLMs

--- Prompt Engineering/ReAct_chain.py
+++ Prompt Engineering/ReAct_chain.py
@@ -21,4 +21,70 @@ def get_product_price(product):
         return 500
     else:
         return 0
-    
+
+def calculator(expression):
+    try:
+        return eval(expression)
+    except:
+        return "calculation error!"
+
+tools = {
+    "get_product_price": get_product_price,
+    "calculator": calculator
+}
+
+system_prompt = """
+You are a shopping assistant.
+
+You have these tools:
+
+get_product_price(product)
+calculator(expression)
+IMPORTANT:
+Call tools exactly like these examples:
+
+Action: get_product_price("iPhone 17")
+Action: calculator("5000-1000")
+
+Never Write:
+get_product_price(product="iPhone 17")
+
+Never Write:
+calculator(expression="5000-1000")
+
+Follow these rules:
+
+1. Decide what you need to do next.
+2. Call only one tool at a time.
+3. After writing an Action, STOP immediately.
+4. Never guess or invent a tool result.
+5. Wait untill you recieve an Observation.
+6. Then decide your next action.
+7. When the task is complete, give the Final Answer.
+
+Format:
+
+Thought: what you need to do
+Action: tool_name(argument)
+
+When finished:
+
+Final Answer: your answer
+"""
+
+def run_agent(question):
+
+    messages = [
+        {
+            "role": "system",
+            "content": system_prompt#the prompt written above
+        },
+        {
+            "role": "user",
+            "content": question
+        }
+    ]
+
+    for step in range(5):
+
+        print("")
