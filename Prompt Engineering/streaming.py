07c3a0b8539ff9ae83a1e51fa907e3896a07f25c
Ankesh04 <osankeshagarwal4@gmail.com>
2026-07-28 23:18:55 +0530

    Learned streaming in LLMs

---
+++ Prompt Engineering/streaming.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+model="llama-3.3-70b-versatile"
+
+prompt = "Ëxplain how internet works."
+message = {
+    "role": "user",
+    "content": prompt
+}
+messages = [message]
+
+# NORMAL REPONSE STREAM = FALSE
+# response1= client.chat.completions.create(model=model, messages=messages)
+# answer = response1.choices[0].message.content
+# print(answer)
+# here reponse tooks time to display but dislayed all at once
+
+stream 
