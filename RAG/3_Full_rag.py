1b9bfb6f38a6e844c8b1311221d25c31af366b12
Ankesh04 <osankeshagarwal4@gmail.com>
2026-08-18 22:09:44 +0530

    Learned to combine rag and embedding

--- RAG/3_Full_rag.py
+++ RAG/3_Full_rag.py
@@ -34,6 +34,11 @@ documents = [
 document_embedding = model.encode(documents)
 # every line converted to array
 
+# here the document_embedding will consist of 7800+ charectors but it is very small content(knowledge base)
+# but what if there are 100s of pdf then it must be of more that 1 cr charectors 
+#to process this little data llm is taking 20 sec then what if 1 cr chrector would be take then it will take 6-7 hrs
+# to conqure this vector db is used
+
 def cosine_similarity(a,b):
     return np.dot(a,b)/(
         np.linalg.norm(a) * np.linalg.norm(b)
@@ -51,7 +56,7 @@ def retrieve(q_embedding):
     return scores[0]
 
 def ask_llm(question,context):
-    sys_prompt=f"""answer in one line only. Answer only based on this context, do not halucinate. Context: {context}"""
+    sys_prompt=f"""Answer in one line only. Answer only based on this context, do not halucinate. Context: {context}"""
     system_message={
         "role":"system",
         "content":sys_prompt
@@ -64,9 +69,6 @@ def ask_llm(question,context):
     response=client.chat.completions.create(model=groqmodel, messages=messages)
     answer=response.choices[0].message.content
     return answer
-
-
-
 
 query= "How much vacation do i get"
 q_embedding=model.encode(query)
